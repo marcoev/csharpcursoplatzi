4f44b36314a343919808373894fc63ec3d74c4f1
Marco Esparza <marcoev@gmail.com>
2020-01-25 13:58:14 -0700

    Inicio

---
+++ Entidades/Escuela.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+
+namespace CoreEscuela.Entidades
+{
+    class Escuela
+    {
+        string nombre;
+        public string Nombre { 
+            get { return nombre; }
+            set { nombre = value.ToUpper(); }
+        }
+
+        public int AñoDeCreacion { get; set; }
+        public string Pais { get; set; }
+        public string Ciudad { get; set; }
+        public TiposEscuela TipoEscuela { get; set; }
+        // public Curso[] Cursos { get; set; }
+        public List<Curso> Cursos { get; set; }
+
+        public Escuela(string nombre, int año) => (Nombre, AñoDeCreacion) = (nombre, año);
+
+        public Escuela(string nombre, int año, TiposEscuela tipo, string pais = "", string ciudad = ""){
+            //Asignación por tuplas
+            (Nombre, AñoDeCreacion) = (nombre, año);
+            this.Pais = pais;
+            this.Ciudad = ciudad;
+        }
+
+        public override string ToString(){
+            return $"Nombre: {Nombre}, Tipo: {TipoEscuela} \nPais: {Pais}, Ciudad: {Ciudad}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using CoreEscuela.Entidades;
+using static System.Console;
+
+namespace Etapa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var escuela = new Escuela("Platzi Academy", 2012, tipo: TiposEscuela.PreEscolar, ciudad: "Bogota");
+            // Curso[] arregloCurso = {
+            //     new Curso(){ Nombre = "101" },
+            //     new Curso(){
+            //         Nombre = "201"
+            //     },
+            //     new Curso(){
+            //         Nombre = "301"
+            //     }
+            // }
+
+            escuela.Pais = "Colombia";
+            escuela.TipoEscuela = TiposEscuela.Primaria;
+            escuela.Ciudad = "Bogota";
+
+            // escuela.Cursos = new Curso[]{
+            //     new Curso(){ Nombre = "101" },
+            //     new Curso(){ Nombre = "201" },
+            //     new Curso(){ Nombre = "301" }
+            // };
+
+            escuela.Cursos = new List<Curso>{
+                new Curso(){ Nombre = "101" },
+                new Curso(){ Nombre = "201" },
+                new Curso(){ Nombre = "301" }
+            };
+
+            escuela.Cursos.Add(new Curso() { Nombre = "102", Jornada = TiposJornada.Mañana });
+
+            var otraColeccion = new List<Curso>{
+                new Curso(){ Nombre = "401" },
+                new Curso(){ Nombre = "501" },
+                new Curso() { Nombre = "501", Jornada = TiposJornada.Tarde },
+                new Curso(){ Nombre = "502" }
+            };
+
+            Curso tmp = new Curso { Nombre = "101-Vacacional", Jornada = TiposJornada.Noche };
+            //otraColeccion.Clear;
+            escuela.Cursos.AddRange(otraColeccion);
+            escuela.Cursos.Add(tmp);
+            ImprimirCursosEscuela(escuela);
+
+            //Predicate<Curso>cursoAEliminar= fnDelegaEliminarCurso;
+            //Enviar una funcion como parametro
+            //escuela.Cursos.RemoveAll(cursoAEliminar);
+
+            //A partir de C# 6 se infiere que es una funcion, y no es necesario hacer la declaración anterior
+            // escuela.Cursos.RemoveAll(fnDelegaEliminarCurso);
+
+            //Ahora con un delegado
+            escuela.Cursos.RemoveAll(delegate (Curso cur)
+            {
+                return cur.Nombre == "301";
+            });
+
+            //Ahora con expresion lambda, queda mas corto el codigo
+            escuela.Cursos.RemoveAll((Curso cur) => cur.Nombre == "501" && cur.Jornada == TiposJornada.Tarde);
+
+            WriteLine("Curso.Hash " + tmp.GetHashCode());
+            escuela.Cursos.Remove(tmp);
+            ImprimirCursosEscuela(escuela);
+
+
+            /*
+            Console.WriteLine(escuela);
+            System.Console.WriteLine("=========");
+            ImprimirCurso(escuela.Cursos);
+            */
+        }
+
+        private static bool fnDelegaEliminarCurso(Curso obj)
+        {
+            return obj.Nombre == "301";
+        }
+
+        /// <SUMMARY>
+        /// Comentarios del metodo
+        private static void ImprimirCursosEscuela(Escuela escuela)
+        {
+            WriteLine("====================");
+            WriteLine("Cursos de la escuela");
+            WriteLine("====================");
+
+            // if (escuela != null && escuela.Cursos != null)
+            if (escuela?.Cursos != null)
+            {
+                foreach (var curso in escuela.Cursos)
+                {
+                    Console.WriteLine($"Curso {curso.Nombre}, Id {curso.UniqueId}");
+                }
+            }
+        }
+
+        private static void ImprimirCurso(Curso[] arregloCurso)
+        {
+            foreach (var item in arregloCurso)
+            {
+                Console.WriteLine($"{item.Nombre}, {item.UniqueId}");
+            }
+        }
+    }
+}
